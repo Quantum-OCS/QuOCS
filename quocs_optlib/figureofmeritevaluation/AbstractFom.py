9a533fcdb5584d041951196a1a6a3c013bbbc445
marcorossignolo <marcorossignolo92@gmail.com>
2021-03-25 17:38:27 +0100

    Comment more Abstract class for figure of merit evaluation

--- quocs_optlib/figureofmeritevaluation/AbstractFom.py
+++ quocs_optlib/figureofmeritevaluation/AbstractFom.py
@@ -2,8 +2,9 @@ from abc import ABCMeta, abstractmethod
 
 
 class AbstractFom(metaclass=ABCMeta):
-    """ Just an abstract class for figure of merit evaluation. Just for test"""
+    """Abstract class for figure of merit evaluation"""
 
     @abstractmethod
-    def get_FoM(self, pulses_list, time_grids_list, parameters_list):
-        pass
+    def get_FoM(self, pulses_list, time_grids_list, parameters_list) -> dict:
+        """Abstract method for figure of merit evaluation. It returns a dictionary with
+         the FoM key inside """
