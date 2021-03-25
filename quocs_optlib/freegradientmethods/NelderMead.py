0e0d2988789c71ee36e711bc8f2290b665ad773a
marcorossignolo <marcorossignolo92@gmail.com>
2021-03-25 18:18:22 +0100

    Update the copyright

--- quocs_optlib/freegradientmethods/NelderMead.py
+++ quocs_optlib/freegradientmethods/NelderMead.py
@@ -1,5 +1,5 @@
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  Copyright [2021] Optimal Control Suite
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Copyright 2021-  QuOCS Team
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import numpy as np
 
 np.seterr(all="raise")
