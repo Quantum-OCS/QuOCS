38940be35aa2772b1ec1491280d8c382393f7bbc
Marco Rossignolo <44845412+marcorossignolo@users.noreply.github.com>
2021-05-06 14:00:18 +0200

    Update conf.py

--- docsource/conf.py
+++ docsource/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/quocslib/'))
+sys.path.insert(0, os.path.abspath('./src/quocslib/'))
 
 
 # -- Project information -----------------------------------------------------
