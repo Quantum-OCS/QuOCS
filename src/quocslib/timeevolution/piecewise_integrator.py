dc3da544be48162cf5418b4ab447426dc506e5ea
Alastair Marshall <40397029+alastair-marshall@users.noreply.github.com>
2022-01-25 11:15:25 +0100

    remove jax

--- src/quocslib/timeevolution/piecewise_integrator.py
+++ src/quocslib/timeevolution/piecewise_integrator.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.linalg import expm
 
 # can we do conditional import?
-import jax.scipy as jsp
+# import jax.scipy as jsp
 
 def pw_evolution(U_store, drive, A, B, n_slices, dt):
     K = len(B)
@@ -20,5 +20,5 @@ def pw_final_evolution(drive, A, B, n_slices, dt, u0):
         H = A
         for k in range(K):
             H = H + drive[k, i] * B[k]
-        U = jsp.linalg.expm(-1j * dt * H) @ U
+        U = expm(-1j * dt * H) @ U
     return U
