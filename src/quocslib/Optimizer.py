2a35076b135b75dec57360fc0a7a6d2fa802c4ec
Bzzzt90 <tblacky@web.de>
2022-03-30 17:00:39 +0200

    Fixed Optimizer and added file for testing

---
+++ src/quocslib/Optimizer.py
@@ -0,0 +1,57 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Copyright 2021-  QuOCS Team
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from quocslib.handleexit.HandleExit import HandleExit
+from quocslib.utils.dynamicimport import dynamic_import
+from quocslib.utils.inputoutput import readjson
+from quocslib.communication.AllInOneCommunication import AllInOneCommunication
+from quocslib.utils.BestDump import BestDump
+from quocslib.utils.AbstractFoM import AbstractFoM
+
+
+
+class Optimizer:
+
+    def __init__(self,
+                 optimization_dict: dict = None,
+                 FoM_object: AbstractFoM = None):
+        """
+        Write this docstring
+        """
+
+        self.interface_job_name = optimization_dict.setdefault("optimization_client_name", "run")
+
+        self.communication_obj = AllInOneCommunication(interface_job_name=self.interface_job_name,
+                                                  FoM_obj=FoM_object, handle_exit_obj=HandleExit(),
+                                                  dump_attribute=BestDump)
+
+        self.results_path = self.communication_obj.results_path
+
+        self.optimizer_attribute = dynamic_import(
+            attribute=optimization_dict.setdefault("opti_algorithm_attribute", None),
+            module_name=optimization_dict.setdefault("opti_algorithm_module", None),
+            class_name=optimization_dict.setdefault("opti_algorithm_class", None)
+        )
+
+        self.opt_alg_obj = self.optimizer_attribute(optimization_dict=optimization_dict,
+                                                    communication_obj=self.communication_obj)
+
+
+    def execute(self):
+
+        self.opt_alg_obj.begin()
+        self.opt_alg_obj.run()
+        self.opt_alg_obj.end()
---
+++ tests/muh.py
@@ -0,0 +1,55 @@
+import os, sys
+
+from quocslib.optimalcontrolproblems.OneQubitProblem import OneQubit
+from quocslib.utils.AbstractFoM import AbstractFoM
+from quocslib.utils.inputoutput import readjson
+from quocslib.Optimizer import Optimizer
+from scipy.optimize import rosen
+import numpy as np
+import pytest
+
+
+class RosenFoM(AbstractFoM):
+
+    def __init__(self, args_dict:dict = None):
+        if args_dict is None:
+            args_dict = {}
+
+        self.FoM_list = []
+        self.param_list = []
+        self.save_path = ""
+
+    # def __del__(self):
+        # np.savetxt(os.path.join(self.save_path, 'FoM.txt'), self.FoM_list)
+        # np.savetxt(os.path.join(self.save_path, 'params.txt'), self.param_list)
+
+
+    def set_save_path(self, save_path: str = ""):
+        self.save_path = save_path
+
+
+    def get_FoM(self, pulses: list = [], parameters: list = [], timegrids: list = []) -> dict:
+
+        FoM = rosen(np.asarray(parameters))
+
+        self.FoM_list.append(FoM)
+        self.param_list.append(parameters)
+        
+        return {"FoM": FoM}
+
+
+
+
+folder = os.path.dirname(os.path.realpath(__file__))
+optimization_dictionary = readjson(os.path.join(folder, "opt_Rosen_NM.json"))[1]
+
+FoM_object = RosenFoM()
+
+optimization_obj = Optimizer(optimization_dictionary, FoM_object)
+
+FoM_object.set_save_path(optimization_obj.results_path)
+
+optimization_obj.execute()
+
+
+
