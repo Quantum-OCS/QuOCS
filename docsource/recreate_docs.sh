2c0628e568bf19115863e1cf0d62ce1dc5786439
ThomasReisser90 <tblacky@web.de>
2022-04-06 16:34:32 +0200

    removed bash file for docs creation and updated version number

--- docsource/recreate_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ~/anaconda3/etc/profile.d/conda.sh
-
-rm -r source/*
-rm -r ../docs/doctrees/*
-rm -r ../docs/html/*
-
-conda activate && sphinx-apidoc -f -o source/ ../src/quocslib/
-conda activate && make github
