b8530e98bb46a24c51bf555fd7d661736707e822
ThomasReisser90 <tblacky@web.de>
2023-02-28 15:32:59 +0100

    wrote second test for drift compensation

--- tests/test_dCRAB_drift_compensation.py
+++ tests/test_dCRAB_drift_compensation.py
@@ -92,11 +92,11 @@ def plot_controls(result_path):
     plt.savefig(os.path.join(result_path, save_name + '.png'))
 
 
-def test_drift_compensation():
+def test_drift_compensation_average_3_FoM():
 
     optimization_dictionary = {
         "optimization_client_name":
-        "Check_drift_compensation",
+        "Check_drift_compensation_average_3_FoM",
         "algorithm_settings": {
             "algorithm_name": "dCRAB",
             "super_iteration_number": 3,
@@ -106,6 +106,82 @@ def test_drift_compensation():
                 "compensate_after_SI": True,
                 "compensate_after_minutes": 0.01,
                 "num_average": 3
+            },
+            "dsm_settings": {
+                "general_settings": {
+                    "dsm_algorithm_name": "NelderMead",
+                    "is_adaptive": True
+                },
+                "stopping_criteria": {
+                    "max_eval": 100,
+                }
+            }
+        },
+        "pulses": [{
+            "pulse_name": "Pulse_1",
+            "upper_limit": 15.0,
+            "lower_limit": -15.0,
+            "bins_number": 101,
+            "time_name": "time_1",
+            "amplitude_variation": 0.3,
+            "basis": {
+                "basis_name": "Fourier",
+                "basis_vector_number": 2,
+                "random_super_parameter_distribution": {
+                    "distribution_name": "Uniform",
+                    "lower_limit": 0.1,
+                    "upper_limit": 5.0
+                }
+            },
+            "initial_guess": {
+                "function_type": "lambda_function",
+                "lambda_function": "lambda t: np.pi/3.0 + 0.0*t"
+            }
+        }],
+        "parameters": [],
+        "times": [{
+            "time_name": "time_1",
+            "initial_value": 3.0
+        }]
+    }
+
+    optimization_dictionary.setdefault("optimization_direction", "minimization")
+    # define some parameters for the optimization
+    args_dict = {
+        "initial_state": "[1.0 , 0.0]",
+        "target_state": "[1.0/np.sqrt(2), -1j/np.sqrt(2)]",
+        "optimization_factor": -1.0,
+        "include_drift": True
+    }
+    # Create FoM object
+    FoM_object = OneQubit(args_dict=args_dict)
+
+    # Define Optimizer
+    optimization_obj = Optimizer(optimization_dictionary, FoM_object)
+
+    FoM_object.set_save_path(optimization_obj.results_path)
+
+    optimization_obj.execute()
+
+    FoM_object.save_FoM()
+
+    # Plot the results
+    plot_FoM(FoM_object.save_path, FoM_object.FoM_save_name)
+    plot_controls(FoM_object.save_path)
+
+def test_drift_compensation_single_average():
+
+    optimization_dictionary = {
+        "optimization_client_name":
+        "Check_drift_compensation_single_average",
+        "algorithm_settings": {
+            "algorithm_name": "dCRAB",
+            "super_iteration_number": 3,
+            "max_eval_total": 200,
+            "FoM_goal": 0.00001,
+            "compensate_drift": {
+                "compensate_after_SI": True,
+                "compensate_after_minutes": 0.01,
             },
             "dsm_settings": {
                 "general_settings": {
