fa0210ebee61c263be177d2f8f680a733069f115
marcorossignolo <marcorossignolo92@gmail.com>
2023-04-12 21:21:18 +0200

    Reshaped the ADAlgorithm Class

--- src/quocslib/optimizationalgorithms/ADAlgorithm.py
+++ src/quocslib/optimizationalgorithms/ADAlgorithm.py
@@ -13,21 +13,24 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-from ast import Import
 
 import numpy as np
 
+from quocslib.Optimizer import Optimizer
 from quocslib.tools.randomgenerator import RandomNumberGenerator
 
 try:
     import jax.numpy as jnp
     import jax.scipy as jsp
     import jax.scipy.optimize as sopt
+    import jax
 except:
     raise ImportError
 
 from quocslib.optimizationalgorithms.OptimizationAlgorithm import OptimizationAlgorithm
 from quocslib.Controls import Controls
+import scipy as sp
+from scipy import optimize
 from quocslib.timeevolution.piecewise_integrator import pw_final_evolution
 
 
@@ -40,6 +43,7 @@ class ADAlgorithm(OptimizationAlgorithm):
     * _get_controls : return the set of controls as a dictionary with pulses, parameters, and times as keys
     * _get_final_results: return the final result of the optimization algorithm
     """
+
     def __init__(self, optimization_dict: dict = None, communication_obj=None, FoM_object=None, **kwargs):
         """
         This is the implementation of the GRAPE algorithm. All the arguments in the constructor are passed to the
@@ -50,11 +54,12 @@ class ADAlgorithm(OptimizationAlgorithm):
         # Optimal algorithm variables if any
         ###########################################################################################
         # Get functions and variable for the gradient optimization
-        self.propagator_func = FoM_object.get_propagator
-        self.initial_state = FoM_object.get_initial_state()
-        self.target_state = FoM_object.get_target_state()
-        self.drift_Hamiltonian = FoM_object.get_drift_Hamiltonian()
-        self.control_Hamiltonians = FoM_object.get_control_Hamiltonians()
+        # self.propagator_func = FoM_object.get_propagator
+        # self.initial_state = FoM_object.get_initial_state()
+        # self.target_state = FoM_object.get_target_state()
+        # self.drift_Hamiltonian = FoM_object.get_drift_Hamiltonian()
+        # self.control_Hamiltonians = FoM_object.get_control_Hamiltonians()
+        self.FoM_object = FoM_object
         self.FoM_list = []
         self.sys_type = optimization_dict.setdefault("sys_type", "StateTransfer")
         # set stopping criteria
@@ -86,6 +91,7 @@ class ADAlgorithm(OptimizationAlgorithm):
             optimization_dict["times"],
             optimization_dict["parameters"],
             rng=self.rng,
+            is_AD=True,
         )
 
         ###########################################################################################
@@ -137,13 +143,64 @@ class ADAlgorithm(OptimizationAlgorithm):
     #         self.sys_type,
     #     )
 
-
-    def inner_routine_call(self, optimized_control_parameters: np.array):
+    def inner_routine_call(self, optimized_control_parameters: jnp.array):
         """Function evaluation call for the L-BFGS-B algorithm"""
         FoM = self._routine_call(optimized_control_parameters=optimized_control_parameters, iterations=0)
         return FoM
 
-    def run(self) -> None:
+    def value_grad(self):
+        """ Return value and grad with jax """
+        jax_function = jax.value_and_grad(self.inner_routine_call)
+        return jax_function
+
+    def get_gradient(self, optimized_control_parameters: np.array):
+        """ Calculate the value and the gradient of the specific function """
+        # Convert in jax object
+        # optimized_control_parameters_jax = jnp.asarray(optimized_control_parameters)
+        #
+        [pulses, timegrids, parameters] = self.controls.get_controls_lists(optimized_control_parameters)
+        return self.FoM_object.get_FoM(pulses=pulses, parameters=parameters, timegrids=timegrids)["FoM"]
+        # return jax.value_and_grad()
+
+    def run(self):
+        """ Inner routine call that return the FoM to the algorithm """
+        heuristic_coeff = 5.0
+        random_variation = heuristic_coeff * 2 * (0.5 - self.rng.get_random_numbers(self.controls.get_control_parameters_number()))
+        # Scale it accordingly to the amplitude variation
+        initial_variation = random_variation * self.controls.get_sigma_variation()
+        # Define the initial
+        init_xx = self.controls.get_mean_value() + initial_variation
+        # Optimize the function
+        # results = optimize.minimize(self.value_and_grad,
+        #                             x0=init_xx,
+        #                             jac=True,
+        #                             method='L-BFGS-B',
+        #                             options={'disp': True})
+        # # Try with the BFGS
+        # results = optimize.minimize(self.inner_routine_call,
+        #                             x0=init_xx,
+        #                             jac=self.get_gradient,
+        #                             method='BFGS',
+        #                             options={'disp': True})
+
+        # # Try with the BFGS
+        # results = optimize.minimize(self.value_grad(),
+        #                             x0=init_xx,
+        #                             jac=True,
+        #                             method='L-BFGS-B', #method='BFGS',
+        #                             options={'disp': True})
+
+        # get_gradient = lambda x: np.array(jax.jit(self.inner_routine_call)(x))
+        get_gradient = lambda x: np.array(jax.jit(jax.grad(self.inner_routine_call))(x))
+        results = optimize.minimize(self.inner_routine_call,
+                                    x0=init_xx,
+                                    jac=get_gradient,
+                                    method='L-BFGS-B',
+                                    options={'disp': True})
+
+        print(results)
+
+    def run_legacy(self) -> None:
         """This runs the main loop of the optimization, assuming that everything
         has been configured correctly this should use LBFGS, or a chosen algorithm,
         to optimize the pulse
@@ -220,3 +277,78 @@ class ADAlgorithm(OptimizationAlgorithm):
             self.FoM_list.append(FoM)
             self.iteration_number_list.append(self.iteration_number)
         return response_dict
+
+    def end(self) -> None:
+        """Finalize the transmission with  the client"""
+        # Check client update
+        self.comm_obj.check_msg_client()
+        # End communication
+        # ToDo: fix this... this is kind of a workaround for now
+        end_comm_message_dict = self._get_final_results()
+        # self.comm_obj.dump_obj.dump_dict("optimized_parameters", end_comm_message_dict)
+        if self.higher_order_terminate_reason != "":
+            end_comm_message_dict["Termination Reason"] = self.higher_order_terminate_reason
+        self.comm_obj.end_communication(end_comm_message_dict)
+        # Update server message
+        self.comm_obj.update_msg_server()
+
+
+def main(optimization_dictionary: dict, args_dict: dict):
+    from quocslib.optimalcontrolproblems.RosenbrockProblem import Rosenbrock
+    # Create FoM object
+    FoM_object = IsingModel(args_dict=args_dict)
+
+    # Define Optimizer
+    optimization_obj = Optimizer(optimization_dictionary, FoM_object)
+    optimization_obj.execute()
+
+    opt_alg_obj = optimization_obj.get_optimization_algorithm()
+    # Get the final results
+    FoM = (opt_alg_obj._get_final_results())["Figure of merit"]
+    # Get the best controls and check if they correspond to the best FoM
+    opt_alg_obj = optimization_obj.get_optimization_algorithm()
+    controls = opt_alg_obj.get_best_controls()
+    FoM_check = FoM_object.get_FoM(**controls)["FoM"]
+    # Check if the FoM calculated during the optimization is consistent with the one calculated after the optimization
+    # using the best controls
+    assert (np.abs(FoM - FoM_check) < 10 ** (-8))
+
+
+if __name__ == "__main__":
+    from quocslib.optimalcontrolproblems.IsingModelADProblem import IsingModel
+
+    optimization_dictionary = {
+        "Disclaimer":
+            "Do not use this json file for optimization",
+        "optimization_client_name":
+            "Optimization_AD_IsingModel",
+        "algorithm_settings": {
+            "algorithm_name": "AD"
+        },
+        "pulses": [{
+            "pulse_name": "Pulse_1",
+            "upper_limit": 100.0,
+            "lower_limit": -100.0,
+            "bins_number": 100,
+            "amplitude_variation": 20.0,
+            "time_name": "time_1",
+            "basis": {
+                "basis_name": "PiecewiseBasis",
+                "bins_number": 100
+            },
+            "initial_guess": {
+                "function_type": "lambda_function",
+                "lambda_function": "lambda t: 0.0 + 0.0*t"
+            }
+        }],
+        "parameters": [],
+        "times": [{
+            "time_name": "time_1",
+            "initial_value": 1.0
+        }]
+    }
+
+    optimization_dictionary.setdefault("optimization_direction", "minimization")
+    # define some parameters for the optimization
+    args_dict = {}
+    main(optimization_dictionary, args_dict)
