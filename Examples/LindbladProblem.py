aff0c1cbaf7c3b9c4dfbbd7b28c4c7dfd5e61813
ThomasReisser90 <tblacky@web.de>
2023-04-21 16:15:08 +0200

    Made a working example using Lindblad and AD

--- Examples/LindbladProblem.py
+++ Examples/LindbladProblem.py
@@ -18,6 +18,7 @@ import jax
 import numpy as np
 import jax.numpy as jnp
 import jax.scipy as jsp
+from jax.scipy.linalg import sqrtm
 from quocslib.utils.AbstractFoM import AbstractFoM
 from functools import partial
 import sys
@@ -26,7 +27,8 @@ import sys
 class TLSProblem(AbstractFoM):
     """
     This class tackles the Lindblad problem for a single qubit driven about the x-axis under decay to the ground state.
-    See the example 6 (equations (17), (96 - 89)) in https://doi.org/10.1063/1.5115323
+    See the example 6 (equations (17) model, (76) time evolution, (96 - 98) specific Liouvillian)
+    in https://doi.org/10.1063/1.5115323
     """
 
     def __init__(self, args_dict: dict = None):
@@ -38,14 +40,14 @@ class TLSProblem(AbstractFoM):
         self.gamma_decay = args_dict.setdefault("gamma_decay", 0.1)
         self.E = args_dict.setdefault("E", 1)
         self.rho_0 = jnp.array([[1, 0], [0, 0]], dtype=jnp.complex64)
-        self.rho_target = jnp.array([[0.5, 0.5], [0.5, 0.5]], dtype=jnp.complex64)
+        self.rho_target = jnp.array([[0.5, 0.5j], [-0.5j, 0.5]], dtype=jnp.complex64)
 
         self.rho_0_LFS = convert_rho_to_LFS(self.rho_0)
         self.rho_target_LFS = convert_rho_to_LFS(self.rho_target)
 
         self.L_const = jnp.array([[0, 0, 0, self.gamma_decay],
-                                  [0, -1.0j*self.E-self.gamma_decay/2, 0, 0],
-                                  [0, 0, -1.0j*self.E-self.gamma_decay/2, 0],
+                                  [0, -1.0j*self.E+self.gamma_decay/2, 0, 0],
+                                  [0, 0, 1.0j*self.E-self.gamma_decay/2, 0],
                                   [0, 0, 0, -self.gamma_decay]],
                                  dtype=jnp.complex64)
 
@@ -86,7 +88,7 @@ def fidelity_funct(rho_evolved, rho_aim):
     :param rho_aim:
     :return: fidelity
     """
-    return jnp.abs(jnp.trace(jnp.sqrtm(jnp.sqrtm(rho_evolved) @ rho_aim @ jnp.sqrtm(rho_evolved)))) ** 2
+    return jnp.abs(jnp.trace(sqrtm(sqrtm(rho_evolved) @ rho_aim @ sqrtm(rho_evolved)))) ** 2
 
 
 def fidelity_funct_LFS(rho_evolved, rho_aim):
@@ -109,6 +111,16 @@ def convert_rho_to_LFS(rho):
     return rho_LFS
 
 
+def convert_LFS_to_rho(rho_LFS):
+    """
+    Function to convert a Fock-Liouville space (FLS) to a density matrix.
+    :param rho_LFS:
+    :return: rho
+    """
+    rho = jnp.array([[rho_LFS[0], rho_LFS[1]], [rho_LFS[2], rho_LFS[3]]], dtype=jnp.complex64)
+    return rho
+
+
 # @partial(jax.jit, static_argnames=["rho_0", "L_const", "L_drive"])
 @jax.jit
 def pw_evolution_final(rho_0, L_const, L_drive, drive, time_grid):
@@ -120,13 +132,9 @@ def pw_evolution_final(rho_0, L_const, L_drive, drive, time_grid):
 
     def body_fun(i, val):
         L_curr = L_const + L_drive * drive[i]
-        U_curr = jsp.linalg.expm(-1.0j * L_curr * dt)
+        U_curr = jsp.linalg.expm(L_curr * dt)
         return U_curr @ val
 
     rho_evol = jax.lax.fori_loop(0, len(time_grid), body_fun, rho)
 
     return rho_evol
-
-
-# muh = TLSProblem()
-# muh.get_FoM(pulses=[np.array([0.1]*20)], parameters=[], timegrids=[np.linspace(0, 1, 20)])
