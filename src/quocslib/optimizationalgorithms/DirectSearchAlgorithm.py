2d703924f17221dc0903d8090d7e7bb184e1537e
ThomasReisser90 <tblacky@web.de>
2023-04-24 13:33:01 +0200

    Added re-eval steps and drift compensation to DirectSearchAlgorithm

--- src/quocslib/optimizationalgorithms/DirectSearchAlgorithm.py
+++ src/quocslib/optimizationalgorithms/DirectSearchAlgorithm.py
@@ -19,6 +19,8 @@ from quocslib.Controls import Controls
 from quocslib.gradientfreemethods.NelderMead import NelderMead
 from quocslib.utils.dynamicimport import dynamic_import
 from quocslib.tools.linearalgebra import simplex_creation
+import numpy as np
+from scipy.stats import norm
 
 
 class DirectSearchAlgorithm(OptimizationAlgorithm):
@@ -58,6 +60,34 @@ class DirectSearchAlgorithm(OptimizationAlgorithm):
             self.dsm_obj = dsm_attribute(direct_search_method_settings,
                                          stopping_criteria,
                                          callback=self.is_optimization_running)
+
+        ################################################################################################
+        # Extract some Settings for the Search #########################################################
+        ################################################################################################
+        alg_parameters = optimization_dict["algorithm_settings"]
+        drift_compensations_parameters = optimization_dict["algorithm_settings"].setdefault("compensate_drift", {})
+        # Define sigma for re-evaluation steps
+        self.best_sigma = 0.0
+        # Update the FoM for drift
+        self.compensate_drift_after_minutes = drift_compensations_parameters.setdefault("compensate_after_minutes", 0.0)
+        self.compensate_drift_num_average = drift_compensations_parameters.setdefault("num_average", 1)
+        # Re-evaluation steps option
+        if "re_evaluation" in alg_parameters:
+            re_evaluation_parameters = alg_parameters["re_evaluation"]
+            if "re_evaluation_steps" in re_evaluation_parameters:
+                self.re_evaluation_steps = np.asarray(re_evaluation_parameters["re_evaluation_steps"], dtype=float)
+            else:
+                self.re_evaluation_steps = np.asarray([0.3, 0.5, 0.6], dtype=float)
+                message = "Steps not found. The default will be used in the optimization: {0}".format(
+                    self.re_evaluation_steps)
+                self.comm_obj.print_logger(message, level=30)
+            # Define the FoM test and sigma test arrays, with the maximum number of steps
+            # FoM test and sigma test are arrays containing the FoM and sigma at every re-evaluation step
+            self.FoM_test = np.zeros(self.re_evaluation_steps.shape[0] + 1, dtype=float)
+            self.sigma_test = np.zeros_like(self.FoM_test)
+        else:
+            self.re_evaluation_steps = None
+
         ###########################################################################################
         # Optimal algorithm variables ###########################################################
         ###########################################################################################
@@ -70,6 +100,144 @@ class DirectSearchAlgorithm(OptimizationAlgorithm):
                                  optimization_dict["parameters"])
         self.FoM_list: list = []
         self.iteration_number_list: list = []
+
+    def _inner_routine_call(self, optimized_control_parameters: np.array, iterations: int,
+                            drift_comp=False, drift_comp_average=1) -> float:
+        """
+        Wrapper Function for the _routine_call of the OptimizationAlgorithm class.
+        Used to perform drift compensation and re-evaluation steps.
+        """
+        # set the is_record to False unless we have drift_comp call... then we want to update the current best FoM
+        self.is_record = False
+        # Initialize step number to 0
+        self.step_number = 0
+        FoM = -1.0 * self.optimization_factor * self._routine_call(optimized_control_parameters, iterations)
+        # This has to be here because it takes the hard-coded initial FoM of 1e10 if the optimization is stopped
+        # before the first search iteration has finished
+        if self.optimization_direction == "maximization" and FoM == 1e10:
+            FoM = -FoM
+        ################################################################################################################
+        # Standard function evaluation - dCRAB without re-evaluation steps
+        ################################################################################################################
+        if self.re_evaluation_steps is None:
+            mu_1 = FoM
+        else:
+            ############################################################################################################
+            # Implement the re-evaluation step method
+            ############################################################################################################
+            # check mu-sig criterion by calculating probability of current pulses being new record
+            # Re evaluation steps initialization e.g. [0.33, 0.5, 0.501, 0.51]
+            re_evaluation_steps = self.re_evaluation_steps
+            # First evaluation in whole optimization -> do not reevaluate
+            if self.iteration_number == 1:
+                re_evaluation_steps = np.array([0.5])
+            # number of steps
+            max_steps_number = re_evaluation_steps.shape[0]
+            # Initialize to zero the FoM_test and the sigma_test arrays
+            self.FoM_test = 0.0 * self.FoM_test
+            self.sigma_test = 0.0 * self.sigma_test
+            # Get the figure of merit from the client
+            self.FoM_test[0] = FoM
+            # TODO: Check if optimization_is_running is necessary here
+            # Get the standard deviation
+            self.sigma_test[0] = float(self.FoM_dict.setdefault("std", 1.0))
+            # Increase step number after function evaluation
+            self.step_number += 1
+            # p level test better than current record
+            for ii in range(max_steps_number):
+                p_level = re_evaluation_steps[ii]
+                mu_1, sigma_1 = self._get_average_FoM_std(mu_sum=np.sum(self.FoM_test) * 1.0,
+                                                          sigma_sum=np.sum(self.sigma_test) * 1.0)
+                mu_2, sigma_2 = self.best_FoM, self.best_sigma
+                probability = self._probabnormx1betterx2(mu_1, sigma_1, mu_2, sigma_2)
+                # If probability is lower than the probability in the list return the
+                if probability < p_level:
+                    return mu_1
+                # else: go on with further re-evaluations
+                self.FoM_test[ii + 1] = -1.0 * self.optimization_factor * self._routine_call(
+                    optimized_control_parameters, iterations)
+                self.sigma_test[ii + 1] = float(self.FoM_dict.setdefault("std", 1.0))
+                # Increase step number after function evaluation
+                self.step_number += 1
+
+            # check if last threshold (re_evaluation_steps[-1]) is surpassed -> new record
+            mu_1, sigma_1 = self._get_average_FoM_std(mu_sum=np.sum(self.FoM_test) * 1.0,
+                                                      sigma_sum=np.sum(self.sigma_test) * 1.0)
+            mu_2, sigma_2 = self.best_FoM, self.best_sigma
+            probability = self._probabnormx1betterx2(mu_1, sigma_1, mu_2, sigma_2)
+            # TODO: Check what best FoM means in this case
+            if probability > re_evaluation_steps[-1]:
+                # We have a new record
+                self.best_sigma, self.best_FoM = sigma_1, mu_1
+                self.is_record = True
+                message = "New record achieved. New best FoM: {0}, std: {1}".format(mu_1, sigma_1)
+                self.comm_obj.print_logger(message, level=20)
+                self.best_xx = self.xx.copy()
+                self.comm_obj.update_controls(is_record=True,
+                                              FoM=self.best_FoM,
+                                              sigma=self.best_sigma,
+                                              # super_it=self.super_it,
+                                              iteration_number=self.iteration_number)
+
+        if drift_comp:
+            self.best_FoM = mu_1
+            self.is_record = True
+            message = "New record due to drift compensation. New best FoM: {0}".format(mu_1)
+            self.comm_obj.print_logger(message, level=20)
+            self.best_xx = self.xx.copy()
+            self.comm_obj.update_controls(is_record=True,
+                                          FoM=self.best_FoM,
+                                          # super_it=self.super_it,
+                                          iteration_number=self.iteration_number)
+
+        # Return the figure of merit to be minimized by the updating algorithm
+        return -1.0 * self.optimization_factor * mu_1
+
+    def _get_average_FoM_std(self, mu_sum: float = None, sigma_sum: float = None) -> np.array:
+        """Calculate the average figure of merit and sigma"""
+        step_number = self.step_number
+        # For the first evaluation and in case no re-evaluation step is needed return directly
+        if step_number == 0:
+            return self.FoM_dict["FoM"], self.FoM_dict.setdefault("std", 1.0)
+        # Call from the response for client function. Calculate the average FoM based on all the previous FoM stored in
+        # the FoM_test array and the current FoM in the FoM dict
+        if mu_sum is None:
+            curr_FoM, curr_std = self.FoM_dict["FoM"], self.FoM_dict.setdefault("std", 1.0)
+            mu_sum = np.mean(self.FoM_test[:step_number]) * (step_number - 1) + curr_FoM
+            sigma_sum = (np.mean(self.sigma_test[:step_number]) * (step_number - 1) + curr_std)
+        # If it is called inside the _inner_routine_call()
+        average_FoM, average_std = mu_sum / step_number, sigma_sum / step_number
+        return average_FoM, average_std
+
+    def _probabnormx1betterx2(self, mu_1: float, sigma_1: float, mu_2: float, sigma_2: float):
+        """
+        Calculates probability for normal distributed random variable x1 being greater or equal than x2
+        x1 usually refers to the test pulse and
+        x2 to the current record that is tried to be outperformed
+        ----------------------------------------------------------------------------------------------------------------
+        :param mu_1: = <x1>
+        :param sigma_1: = std(x1)
+        :param mu_2:  = <x2>
+        :param sigma_2: = std(x2)
+        :return: probability P(x1>=x2)
+        """
+        # Start by defining a new random variable z = x1 - x2
+        # if mu_z > 0 the probability is > 0.5 , else: <0.5
+        mu_z = mu_2 - mu_1
+        if self.optimization_direction == "maximization":
+            mu_z = mu_1 - mu_2
+        std_comb = np.sqrt(sigma_1**2 + sigma_2**2)
+        if np.abs(std_comb) < 10**(-14):
+            # Warning message
+            message = ("Combined standard deviation std_comb = {0} < 10**(-14) . To avoid numerical instabilities "
+                       "std_comb will be set equal to 1.0".format(std_comb))
+            self.comm_obj.print_logger(message, level=30)
+            # Set std_com to 1.0
+            std_comb = 1.0
+        zz = mu_z / std_comb
+        # Calculate the probability with the cumulative density function
+        probability = norm.cdf(zz)
+        return probability
 
     def _get_response_for_client(self):
         """
@@ -89,6 +257,13 @@ class DirectSearchAlgorithm(OptimizationAlgorithm):
             "iteration_number": self.iteration_number,
             "status_code": status_code
         }
+
+        if self.re_evaluation_steps is not None:
+            FoM, std = self._get_average_FoM_std()
+            message = "Function evaluation number: {func_eval}, ".format(func_eval=self.iteration_number)
+            message += "Re-eval. number: {0}, FoM: {1}, std: {2}".format(self.step_number, FoM, std)
+            self.comm_obj.print_logger(message, level=20)
+
         if status_code == 0:
             self.FoM_list.append(FoM)
             self.iteration_number_list.append(self.iteration_number)
@@ -117,10 +292,12 @@ class DirectSearchAlgorithm(OptimizationAlgorithm):
         # Initialize the best xx vector for this SI
         self.best_xx = self.controls.get_mean_value().copy()
         # Run the direct search algorithm
-        result_l = self.dsm_obj.run_dsm(self._routine_call,
+        result_l = self.dsm_obj.run_dsm(self._inner_routine_call,
                                         x0,
                                         initial_simplex=start_simplex,
-                                        sigma_v=self.controls.get_sigma_variation())
+                                        sigma_v=self.controls.get_sigma_variation(),
+                                        drift_comp_minutes=self.compensate_drift_after_minutes,
+                                        drift_comp_num_average=self.compensate_drift_num_average)
         # Update the results
         [self.best_FoM, self.xx,
          self.terminate_reason] = [result_l["F_min_val"], result_l["X_opti_vec"], result_l["terminate_reason"]]
