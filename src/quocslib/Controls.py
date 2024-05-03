d75808b01fde452ad2a1cd00044b537289b1b0d8
MARCO ROSSIGNOLO <marco.rossignolo@qruise.com>
2024-05-03 16:30:09 +0200

    Fixed super parameters append at the end of the superiteration for different basis number pulses

--- src/quocslib/Controls.py
+++ src/quocslib/Controls.py
@@ -158,18 +158,18 @@ class Controls:
         # TODO Implement the time optimization here
         # Return the control parameters number
 
-    def get_random_super_parameter(self) -> np.array:
+    def get_random_super_parameter(self) -> list[float]:
         """
         Returns a list with current super_parameters
 
-        :return np.array: List with current super_parameters
+        :return list: List with current super_parameters
         """
         super_parameter_list = []
         for pulse in self.pulse_objs_list:
             if isinstance(pulse, ChoppedBasis):
-                super_parameter_list.append(pulse.super_parameter_distribution_obj.w)
-        super_parameter_array = np.asarray(super_parameter_list)
-        return super_parameter_array
+                super_parameter_list.append(list(pulse.super_parameter_distribution_obj.w))
+        # super_parameter_array = np.asarray(super_parameter_list)
+        return super_parameter_list
 
     def get_sigma_variation(self) -> np.array:
         """
@@ -228,7 +228,7 @@ class Controls:
         for parameter in self.parameter_objs_list:
             parameter.set_parameter(optimized_parameters_vector[parameter.control_parameters_list])
 
-    def _get_controls_lists(self, optimized_parameters_vector: np.array) -> [list, list, list]:
+    def _get_controls_lists(self, optimized_parameters_vector: np.array) -> list[list[float | np.ndarray]]:
         """
         Sets the optimized control parameters and get the controls
 
